7885fd0dc213005ba2bbf0004cc85e2339c60e0c
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-02-18 18:10:09 +0330

    manifest added

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import withPWA from "next-pwa";
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+// export default nextConfig;
+export default pwaConfig(nextConfig);
